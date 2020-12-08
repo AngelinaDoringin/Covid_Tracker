707e336ae52619b23af57b26eb75b68cb14ff5b1
AngelinaDoringin <s21730034@student.unklab.ac.id>
2020-12-08 10:24:02 +0700

    delete folder tabelProv & buat buat tabel baru

--- src/pages/Provinsi/index.js
+++ src/pages/Provinsi/index.js
@@ -1,19 +1,58 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
+import NumberFormat from "react-number-format";
+
 
 const Provinsi = () =>{
-    const[users, setUsers] = useState([])
-
+    const[users,setUsers] = useState([])
+    
     useEffect(() => {
         axios
          .get("https://indonesia-covid-19.mathdro.id/api/provinsi")
-         .then((response) => setUsers(response.data));
+         .then((response) =>  setUsers(response.data.data) );
     }, []);
-    console.log(users);
-
-            return (
-                <p>Jumlah Kasus Per Provinsi</p>
-            );
+    
+   return(
+     <div>
+       <h3>Jumlah kasus per Provinsi</h3>
+       <div className="center">
+           <table border="1">
+               <tr> 
+                   <td>
+                       Provinsi
+                   </td>
+                   <td>
+                       Positif
+                   </td>
+                   <td>
+                       Sembuh
+                   </td>
+                   <td>
+                        Meninggal
+                   </td>
+               </tr>
+               {users.map((item)=>{
+                   return(
+                       <tr>
+                           <td>
+                               {item.provinsi}
+                           </td>
+                           <td>
+                                <NumberFormat value={item.kasusPosi} displayType={'text'} thousandSeparator={true}/>
+                           </td>
+                           <td>
+                           <NumberFormat value={item.kasusSemb} displayType={'text'} thousandSeparator={true}/>
+                           </td>
+                           <td>
+                           <NumberFormat value={item.kasusMeni} displayType={'text'} thousandSeparator={true}/>
+                           </td>
+                       </tr>
+                   )
+               })
+               }
+           </table>
+        </div>
+    </div>
+   )
 };
-
 export default Provinsi;
